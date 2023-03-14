d47e97145b0bb56cab7bcee0c3231f57d2aab5c6
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-14 16:49:43 -0300

    configured turborepo

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -60,35 +60,20 @@ var colors = {
 };
 var space = {
   1: "0.25rem",
-  /* 4px */
   2: "0.5rem",
-  /* 8px */
   3: "0.75rem",
-  /* 12px */
   4: "1rem",
-  /* 16px */
   5: "1.25rem",
-  /* 20px */
   6: "1.5rem",
-  /* 24px */
   7: "1.75rem",
-  /* 28px */
   8: "2rem",
-  /* 32px */
   10: "2.5rem",
-  /* 40px */
   12: "3rem",
-  /* 48px */
   16: "4rem",
-  /* 64px */
   20: "5rem",
-  /* 80px */
   40: "10rem",
-  /* 160px */
   64: "16rem",
-  /* 256px */
   80: "20rem"
-  /* 320px */
 };
 var radii = {
   px: "1px",
@@ -109,31 +94,18 @@ var fontWeights = {
 };
 var fontSizes = {
   xxs: "0.625rem",
-  /* 10px */
   xs: "0.75rem",
-  /* 12px */
   sm: "0.875rem",
-  /* 14px */
   md: "1rem",
-  /* 16px */
   lg: "1.125rem",
-  /* 18px */
   xl: "1.25rem",
-  /* 20px */
   "2xl": "1.5rem",
-  /* 24px */
   "4xl": "2rem",
-  /* 32px */
   "5xl": "2.25rem",
-  /* 36px */
   "6xl": "3rem",
-  /* 48px */
   "7xl": "4rem",
-  /* 64px */
   "8xl": "4.5rem",
-  /* 72px */
   "9xl": "6rem"
-  /* 96px */
 };
 var lineHeights = {
   shorter: "125%",
