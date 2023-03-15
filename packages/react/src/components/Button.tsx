8dd89cfc004623e4df636b03d864462e89841457
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-14 21:35:07 -0300

    created component Button

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,94 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: 'small',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        backgroundColor: '$ignite500',
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite300',
+        },
+      },
+
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite500',
+          color: '$white',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite500',
+          color: '$white',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
